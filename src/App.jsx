e8b360f446f812724220b67dcbbb76828a33992d
jnarvaez04 <j.narvaez564@gmail.com>
2023-05-01 16:01:45 -0500

    fetch entpoint request

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+
+
+const CAT_ENTPOINT_RANDOM_FACT = `https://catfact.ninja/fact`;
+// const CAT_ENTPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE_URL = `https://cataas.com`;
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [ fact, setFact ] = useState();
+  const [ imageUrl, setImageUrl ] = useState();
+
+  // Para recuperar la cita al cargar la página
+  useEffect(() => {
+    fetch(CAT_ENTPOINT_RANDOM_FACT)
+    .then((res) => res.json())
+    .then(data => {
+      const { fact } = data;
+      setFact( fact )
+    })
+
+  }, [])
+
+
+  // Para recuperar la imagen cada vez que tenemos una cita nueva
+  useEffect(() => {
+    if(!fact) return
+
+    const threeFirstWords = fact.split(' ', 3).join(' ')
+    console.log(threeFirstWords)
+
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`,  { mode: 'no-cors' })
+    .then((res) => res.json())
+    .then(response => {
+      const { url } = response
+      setImageUrl(url)
+     })
+  },[fact])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h2>App de gatitos</h2>
+      { fact && <p>{ fact }</p> }
+      { Image && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the firts three words for ${fact}`}/>}
+    </main>
   )
 }
 
